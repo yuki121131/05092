917d3a19155337f0c7c4ba27c95014ee4116e5c7
yuki <da40825@gmail.com>
2019-05-09 14:11:26 +0800

    second add button

--- app/src/main/java/com/example/cjcu/a0509/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a0509/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cjcu.a0509;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
     }
 }
