958b369197831c406e66f4666330ab222cac3f56
yuki <da40825@gmail.com>
2019-05-09 13:55:32 +0800

    Initial commit

---
+++ app/src/test/java/com/example/cjcu/a0509/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.cjcu.a0509;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
